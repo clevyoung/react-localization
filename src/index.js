6dbbf5c99d0481d6017d58e07ee5c8cdeeefe1b9
clevyoung <clevyoung@gmail.com>
2022-01-12 23:24:31 +0900

    loading fallback 추가

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import i18n from 'i18next';
-import { useTranslation, initReactI18next } from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App';
+
+const loadingMarkup = (
+  <div className='py-4 text-center'>
+    <h3>Loading..</h3>
+  </div>
+);
 
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
+    supportedLngs: ['en', 'fr', 'ar'],
     fallbackLng: 'en',
     detection: {
-      order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
+      order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
       caches: ['cookie'],
-    },
-    resources: {
-      en: {
-        translation: {
-          'Welcome to React': 'Welcome to React and react-i18next',
-        },
-      },
     },
     backend: {
       loadPath: '/assets/locales/{{lng}}/translation.json',
     },
   });
 
-function App() {
-  const { t } = useTranslation();
-
-  return <h2>{t('Welcome to React')}</h2>;
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+  document.getElementById('root')
+);
